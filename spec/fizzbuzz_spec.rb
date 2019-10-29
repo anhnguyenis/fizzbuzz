6fbd3aaf4611fbadc21869493ccba5066db1fbd1
Anh Nguyen <anhnguyenis@gmail.com>
2019-10-29 09:18:39 +0000

    Add second test to spec if number does not return fizz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,15 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+context 'user input returns "fizz"' do
+  describe 'fizzbuzz' do
+    it 'returns "fizz" when passed 3' do
+      expect(fizzbuzz(3)).to eq 'fizz'
+    end
+  end
+
+  describe 'fizzbuzz' do
+    it 'does not return "fizz" when passed 5' do
+      expect(fizzbuzz(5)).to eq 'fizz'
+    end
   end
 end
